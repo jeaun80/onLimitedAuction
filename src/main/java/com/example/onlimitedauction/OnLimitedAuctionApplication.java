19d6c0dc01a95be722ab1984dbfa02cc7b46a4c2
jeaun80 <kims1706@naver.com>
2024-03-07 04:35:57 +0900

    start

---
+++ src/main/java/com/example/onlimitedauction/OnLimitedAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlimitedauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLimitedAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnLimitedAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlimitedauction/OnLimitedAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlimitedauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnLimitedAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
