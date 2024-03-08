343ac8630293ec1a82ea51670964e168bd63a150
jeaun80 <kims1706@naver.com>
2024-03-09 04:49:39 +0900

    [seeting] DB connet setting

--- src/test/java/com/example/onlimitedauction/OnLimitedAuctionApplicationTests.java
+++ src/test/java/com/example/onlimitedauction/OnLimitedAuctionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnLimitedAuctionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
